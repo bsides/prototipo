fd8b54c000e29fee2fbaeb1d26d308c472305209
Rafael Pereira <rafaelbsides@gmail.com>
2014-05-20 16:30:35 -0300

    New build is up with new navbar

--- app/scripts/plugins.js
+++ app/scripts/plugins.js
@@ -1,24 +1,38 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+  var method;
+  var noop = function () {};
+  var methods = [
+  'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+  'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+  'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+  'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+      // Only stub undefined methods.
+      if (!console[method]) {
+        console[method] = noop;
+      }
     }
-}());
+  }());
 
 // Place any jQuery/helper plugins in here.
+(function() {
+  var fontSize = $('body').css('font-size');
+  var plusFontSize = parseFloat(fontSize) * 1.1;
+  var minusFontSize = parseFloat(fontSize) * 0.9;
+  $('#fontIncrease').on('click', function() {
+    $('body').css('font-size', plusFontSize + "px");
+  });
+  $('#fontReset').on('click', function() {
+    $('body').css('font-size', fontSize);
+  });
+  $('#fontDecrease').on('click', function() {
+    $('body').css('font-size', minusFontSize + "px");
+  });
+});
